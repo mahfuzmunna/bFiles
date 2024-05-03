70e235f59889a2682e446c5bf28378bb44590106
mahfuzmunna <mahfuzmunna17@proton.me>
2024-05-03 21:03:58 +0600

    update -> storageOverview_Container&Item

--- app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/BFilesStorageOverview.kt
+++ app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/BFilesStorageOverview.kt
@@ -1,17 +1,90 @@
 package me.mahfuzmunna.bfiles.designsystem.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddToDrive
+import androidx.compose.material.icons.filled.Diamond
+import androidx.compose.material.icons.filled.Folder
+import androidx.compose.material.icons.outlined.Diamond
+import androidx.compose.material.icons.outlined.Folder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.mahfuzmunna.bfiles.designsystem.theme.BFilesTheme
 
 @Composable
 fun BFilesStorageOverviewContainer(
     storageItems: @Composable ColumnScope.() -> Unit
 ) {
-    Column(content = storageItems)
+    Column(
+        content = storageItems,
+        modifier = Modifier,
+        verticalArrangement = Arrangement.SpaceAround
+    )
 }
 
 @Composable
-fun ColumnScope.BFilesStorageOverviewItem() {
+fun BFilesStorageOverviewItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    subtitle: String,
+    leadingIcon: ImageVector,
+    trailingIcon: ImageVector,
+    onTrailingClick: () -> Unit
+) {
+    ListItem(
+        headlineContent = {
+            Text(text = title)
 
+        },
+        modifier = modifier,
+        leadingContent = {
+            Icon(imageVector = leadingIcon, contentDescription = "leadingIcon")
+        },
+        supportingContent = {
+            Text(text = subtitle)
+        },
+        trailingContent = {
+            IconButton(onClick = { onTrailingClick() }) {
+                Icon(imageVector = trailingIcon, contentDescription = "trailingIcon")
+            }
+        },
+        shadowElevation = 4.dp,
+    )
+}
+
+
+@Preview
+@Composable
+private fun BFilesStorageOverviewContentPreview() {
+    BFilesTheme {
+        BFilesStorageOverviewContainer {
+            BFilesStorageOverviewItem(
+                title = "Internal Storage",
+                subtitle = "27 GB / 128 GB",
+                leadingIcon = Icons.Filled.Folder,
+                trailingIcon = Icons.Filled.Diamond
+            ) {}
+            BFilesStorageOverviewItem(
+                title = "External Storage",
+                subtitle = "2.5 GB / 32 GB",
+                leadingIcon = Icons.Filled.Folder,
+                trailingIcon = Icons.Outlined.Diamond
+            ) {}
+            BFilesStorageOverviewItem(
+                title = "Google Drive",
+                subtitle = "1.2 GB / 15 GB",
+                leadingIcon = Icons.Filled.AddToDrive,
+                trailingIcon = Icons.Outlined.Diamond
+            ) {}
+        }
+    }
 }
--- app/src/main/java/me/mahfuzmunna/bfiles/feature/home/HomeScreen.kt
+++ app/src/main/java/me/mahfuzmunna/bfiles/feature/home/HomeScreen.kt
@@ -2,24 +2,14 @@ package me.mahfuzmunna.bfiles.feature.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import me.mahfuzmunna.bfiles.designsystem.component.BFilesStorageOverviewContainer
 import me.mahfuzmunna.bfiles.designsystem.component.BFilesStorageOverviewItem
-import me.mahfuzmunna.bfiles.designsystem.component.BFilesTopAppBar
 import me.mahfuzmunna.bfiles.designsystem.theme.BFilesTheme
-import me.mahfuzmunna.bfiles.ui.extension.addGradientToBox
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,11 +19,11 @@ fun HomeScreen() {
 //            .addGradientToBox(MaterialTheme.colorScheme)
             .fillMaxSize()
     ) {
-        Column {
-            BFilesStorageOverviewContainer {
-                BFilesStorageOverviewItem()
-            }
-        }
+//        Column {
+//            BFilesStorageOverviewContainer {
+//                BFilesStorageOverviewItem()
+//            }
+//        }
     }
 }
 
