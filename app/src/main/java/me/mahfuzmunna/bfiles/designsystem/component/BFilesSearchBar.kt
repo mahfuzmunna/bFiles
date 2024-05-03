1ba9f4362814588e187d9ef626cbf8565b5bfd67
mahfuzmunna <mahfuzmunna17@proton.me>
2024-05-03 18:16:03 +0600

    added -> searchbarcomponent

---
+++ app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/BFilesSearchBar.kt
@@ -0,0 +1,9 @@
+package me.mahfuzmunna.bfiles.designsystem.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BFilesSearchBar(modifier: Modifier = Modifier) {
+
+}
