8c6cf9a100e3d2350b87c12fad5798ae718973ed
mahfuzmunna <mahfuzmunna17@proton.me>
2024-05-04 19:31:22 +0600

    added -> navigation +route params -> myFiles

--- app/src/main/java/me/mahfuzmunna/bfiles/feature/filesystem/navigation/MyFilesNavigation.kt
+++ app/src/main/java/me/mahfuzmunna/bfiles/feature/filesystem/navigation/MyFilesNavigation.kt
@@ -1,2 +1,31 @@
 package me.mahfuzmunna.bfiles.feature.filesystem.navigation
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+const val FILES_PATH_ARG = "pathUri"
+const val MY_FILES_ROUTE_BASE = "my_files_route"
+const val MY_FILES_ROUTE = "$MY_FILES_ROUTE_BASE?$FILES_PATH_ARG=$FILES_PATH_ARG"
+fun NavController.navigateToMyFiles(filesPathUri: String? = null) {
+    val route =
+        if (filesPathUri != null) "$MY_FILES_ROUTE_BASE?$FILES_PATH_ARG=$filesPathUri" else MY_FILES_ROUTE_BASE
+    navigate(route)
+}
+
+fun NavGraphBuilder.myFilesScreen() {
+    composable(
+        route = MY_FILES_ROUTE,
+        arguments = listOf(
+            navArgument(FILES_PATH_ARG) {
+                defaultValue = null
+                nullable = true
+                type = NavType.StringType
+            }
+        )
+    ) {
+
+    }
+}
