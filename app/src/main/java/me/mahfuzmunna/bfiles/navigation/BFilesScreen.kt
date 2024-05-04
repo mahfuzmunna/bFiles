ec47b6019deb850c5a623e91554d05ed531c9190
mahfuzmunna <mahfuzmunna17@proton.me>
2024-05-04 19:58:46 +0600

    removed -> sealed class screens now screens route are handled by each feature navigation

--- app/src/main/java/me/mahfuzmunna/bfiles/navigation/BFilesScreen.kt
+++
@@ -1,7 +0,0 @@
-package me.mahfuzmunna.bfiles.navigation
-
-sealed class BFilesScreen(val routeName : String) {
-    object Overview : BFilesScreen("overview_screen")
-    object MyFiles : BFilesScreen("myfiles_screen")
-    object Extensions : BFilesScreen("extensions_screen")
-}
