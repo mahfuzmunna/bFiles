2312f777f2d65f1e0bd50c2f71f71fd85825c667
mahfuzmunna <mahfuzmunna17@proton.me>
2024-05-06 04:24:00 +0600

    added -> extensions screen

--- app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/Extensions.kt
+++ app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/Extensions.kt
@@ -1,11 +1,18 @@
 package me.mahfuzmunna.bfiles.designsystem.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun ExtensionsListContainer(modifier: Modifier = Modifier) {
-
+fun ExtensionsListContainer(modifier: Modifier = Modifier, title : String, content : @Composable ColumnScope.() -> Unit) {
+    Column {
+        Text(text = title)
+        Card(content = content)
+    }
 }
 
 @Composable
--- app/src/main/java/me/mahfuzmunna/bfiles/feature/extensions/ExtensionsScreen.kt
+++ app/src/main/java/me/mahfuzmunna/bfiles/feature/extensions/ExtensionsScreen.kt
@@ -1,9 +1,28 @@
 package me.mahfuzmunna.bfiles.feature.extensions
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import me.mahfuzmunna.bfiles.R
+import me.mahfuzmunna.bfiles.designsystem.component.ExtensionsListContainer
+import me.mahfuzmunna.bfiles.designsystem.component.ExtensionsListItem
 
 @Composable
 fun ExtensionsRoute(modifier: Modifier = Modifier) {
+    ExtensionsScreen()
+}
 
+@Composable
+fun ExtensionsScreen(modifier: Modifier = Modifier) {
+    Column {
+        ExtensionsListContainer(
+            modifier = modifier,
+            title = stringResource(id = R.string.feature_extensions_title)
+        ) {
+            ExtensionsListItem(
+
+            )
+        }
+    }
 }
