6ede0efe0c848f9db7d0223e5e923628e86af7df
mahfuzmunna <mahfuzmunna17@proton.me>
2024-05-05 15:11:39 +0600

    added -> skeleton quickaccess composable

--- app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/QuickAccess.kt
+++ app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/QuickAccess.kt
@@ -1,2 +1,14 @@
 package me.mahfuzmunna.bfiles.designsystem.component
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BFilesQuickAccessContainer(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun BFilesQuickAccessItem(modifier: Modifier = Modifier) {
+
+}
