c747481b01045406584d022c2c37c57a3a850149
mahfuzmunna <mahfuzmunna17@proton.me>
2024-05-05 18:17:56 +0600

    added -> skeleton extensions listitem composable

---
+++ app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/Extensions.kt
@@ -0,0 +1,14 @@
+package me.mahfuzmunna.bfiles.designsystem.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ExtensionsListContainer(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun ExtensionsListItem(modifier: Modifier = Modifier) {
+
+}
--- app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/QuickAccess.kt
+++ app/src/main/java/me/mahfuzmunna/bfiles/designsystem/component/QuickAccess.kt
@@ -1,14 +1,47 @@
 package me.mahfuzmunna.bfiles.designsystem.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.mahfuzmunna.bfiles.designsystem.theme.BFilesTheme
 
 @Composable
-fun BFilesQuickAccessContainer(modifier: Modifier = Modifier) {
-
+fun BFilesQuickAccessContainer(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column {
+        Text(text = title, modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp))
+        Card(content = content)
+    }
 }
 
 @Composable
 fun BFilesQuickAccessItem(modifier: Modifier = Modifier) {
+    ListItem(
+        headlineContent = {
+            Text(text = "headline")
+        },
+        shadowElevation = 5.dp,
+    )
+}
 
+@Preview
+@Composable
+fun BFilesQuickAccessPreview(modifier: Modifier = Modifier) {
+    BFilesTheme {
+        BFilesQuickAccessContainer(title = "Quick Access") {
+            BFilesQuickAccessItem()
+            BFilesQuickAccessItem()
+            BFilesQuickAccessItem()
+        }
+    }
 }
