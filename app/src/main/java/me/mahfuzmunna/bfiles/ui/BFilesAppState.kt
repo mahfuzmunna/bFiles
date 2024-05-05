7a4e6d3f27d7ad928a0a8bbe059eb57c2112d19a
mahfuzmunna <mahfuzmunna17@proton.me>
2024-05-05 15:01:02 +0600

    updated -> appstate popUpTo OverviewScreen

--- app/src/main/java/me/mahfuzmunna/bfiles/ui/BFilesAppState.kt
+++ app/src/main/java/me/mahfuzmunna/bfiles/ui/BFilesAppState.kt
@@ -3,12 +3,16 @@ package me.mahfuzmunna.bfiles.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
+import me.mahfuzmunna.bfiles.feature.extensions.navigation.EXTENSIONS_ROUTE
 import me.mahfuzmunna.bfiles.feature.extensions.navigation.navigateToExtensions
+import me.mahfuzmunna.bfiles.feature.filesystem.navigation.MY_FILES_ROUTE
 import me.mahfuzmunna.bfiles.feature.filesystem.navigation.navigateToMyFiles
+import me.mahfuzmunna.bfiles.feature.home.navigation.HOME_ROUTE
 import me.mahfuzmunna.bfiles.feature.home.navigation.navigateToHome
 import me.mahfuzmunna.bfiles.navigation.TopLevelDestination
 
@@ -29,10 +33,21 @@ class BFilesAppState(
     val currentDestination: NavDestination?
         @Composable get() = navController.currentBackStackEntryAsState().value?.destination
 
-    val topLevelDestinations : List<TopLevelDestination> = TopLevelDestination.entries
+    val currentTopLevelDestination: TopLevelDestination?
+        @Composable get() = when (currentDestination?.route) {
+            HOME_ROUTE -> TopLevelDestination.HOME
+            MY_FILES_ROUTE -> TopLevelDestination.MY_FILES
+            EXTENSIONS_ROUTE -> TopLevelDestination.EXTENSIONS
+            else -> null
+        }
+
+    val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.entries
 
     fun bFilesNavigateToTopLevelDestinations(topLevelDestination: TopLevelDestination) {
         val topLevelNavOptions = navOptions {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
             launchSingleTop = true
             restoreState = true
 
